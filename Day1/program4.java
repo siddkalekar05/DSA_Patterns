0c0e5e62aa5513ce947f29598c3d53d3ab6dcd5e
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-26 22:39:44 +0530

    Add files via upload

---
+++ Day1/program4.java
@@ -0,0 +1,27 @@
+class ArrayDemo4{
+    static void sort2(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+
+    }
+    
+    public static void main(String[] args){
+        int arr[] = new int[]{12,15,8,9,4,98};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        sort2(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
