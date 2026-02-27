fccb35ee4ec5670362a439c329d05fefe135f212
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-27 23:22:15 +0530

    Add files via upload

---
+++ TwoPointer/Program6.java
@@ -0,0 +1,30 @@
+class TwoPointerDemo6{
+    static void merge(int arr1[],int arr2[]){
+        int i = 0, j = 0;
+
+        while(i < arr1.length && j < arr2.length) {
+            if(arr1[i] <= arr2[j]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+            } else {
+                System.out.print(arr2[j] + " ");
+                j++;
+            }
+        }
+        while(i < arr1.length){
+            System.out.print(arr1[i++] + " ");
+            i++;
+        }
+        while(j < arr2.length){
+            System.out.print(arr2[j++] + " ");
+            j++;
+        }
+    }
+
+    public static void main(String[] args){
+        int arr1[] = new int[]{1,3,5,7,9};
+        int arr2[] = new int[]{2,4,6,8,10};
+        merge(arr1,arr2);
+        
+    }
+}
