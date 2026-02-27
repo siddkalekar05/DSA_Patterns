14e205c1df683e56876bdecf8b9278f19b50042a
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-27 23:22:42 +0530

    Add files via upload

---
+++ TwoPointer/Program7.java
@@ -0,0 +1,39 @@
+class TwoPointerDemo7 {
+
+    static void merge(int arr1[], int arr2[]) {
+        int i = 0, j = 0, k = 0;
+        int arr3[] = new int[arr1.length + arr2.length];
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] < arr2[j]) {
+                arr3[k] = arr1[i];
+                i++;
+            } else {
+                arr3[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < arr1.length) {
+            arr3[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < arr2.length) {
+            arr3[k] = arr2[j];
+            j++;
+            k++;
+        }
+        for (int x : arr3) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr1[] = {1, 3, 5, 7, 9};
+        int arr2[] = {2, 4, 6, 8, 10};
+
+        merge(arr1, arr2);
+    }
+}
