8a441a7bb07ea7ec5d2c48efebaa3711dd5f3971
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-01 22:34:56 +0530

    Add files via upload

---
+++ PrefixSum/Program5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class PrefixDemo5 {
+    static boolean prefix(int arr[],Scanner sc){
+        System.out.println("Enter target sum : ");
+        int target = sc.nextInt();
+        for(int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for(int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if(sum == target)
+                    return true;
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[]{1,4,7,9,12,17};
+        System.out.println(prefix(arr,sc));
+    }
+    
+}
