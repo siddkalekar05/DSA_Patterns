b9be3ada4eb9179784468a2a7c9bf2580665bb00
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-02 22:25:04 +0530

    Update Program6.java

--- PrefixSum/Program6.java
+++ PrefixSum/Program6.java
@@ -1,3 +1,35 @@
+/*
+Problem Statement:
+Find index where left sum equals right sum.
+
+Approach:
+Compute total sum.
+Maintain leftSum.
+RightSum = total - leftSum - arr[i].
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+public class PivotIndex {
+    public static int pivot(int[] arr) {
+        int total = 0;
+        for(int num : arr)
+            total += num;
+
+        int leftSum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(leftSum == total - leftSum - arr[i])
+                return i;
+
+            leftSum += arr[i];
+        }
+
+        return -1;
+    }
+}
+
 class PrefixDemo6 {
     static void pivot(int arr[]){
         int totalsum = 0;
@@ -19,3 +51,4 @@ class PrefixDemo6 {
     }
     
 }
+
