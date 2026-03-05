5d77d990f39ff24e80c4bc464618e8b820e8253f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-05 22:56:07 +0530

    Update Program12.java

--- PrefixSum/Program12.java
+++ PrefixSum/Program12.java
@@ -1,8 +1,24 @@
-class GreaterThanAverage {
+/*
+Problem Statement:
+Count the number of elements in the array
+that are greater than the average of the array.
 
-    public static void main(String[] args) {
+Example:
+Input: {1,2,3,4,5}
+Output: 2
 
-        int[] arr = {1,2,3,4,5};
+Approach:
+First calculate the sum of the array.
+Find the average by dividing sum by length.
+Traverse again and count elements greater than average.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class ArrayDemo13 {
+
+    static void countGreater(int arr[]) {
 
         int sum = 0;
 
@@ -13,12 +29,19 @@ class GreaterThanAverage {
 
         int count = 0;
 
-        for(int num : arr){
+        for(int num : arr) {
 
             if(num > avg)
                 count++;
         }
 
-        System.out.println("Count: " + count);
+        System.out.println("Count : " + count);
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5};
+
+        countGreater(arr);
     }
 }
