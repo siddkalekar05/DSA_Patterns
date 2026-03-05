e3cbe323c2526f68e85dbd7830761305602764db
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-05 22:54:27 +0530

    Update Program7.java

--- PrefixSum/Program7.java
+++ PrefixSum/Program7.java
@@ -1,21 +1,46 @@
-class LeadersArray {
+/*
+Problem Statement:
+Given an array of integers, print all the leaders in the array.
+An element is called a leader if it is greater than all the
+elements to its right side.
 
-    public static void main(String[] args) {
+Example:
+Input:  {16,17,4,3,5,2}
+Output: 17 5 2
 
-        int[] arr = {16,17,4,3,5,2};
+Approach:
+The last element is always a leader because there are no elements on its right.
+Traverse the array from right to left.
+Keep track of the maximum element seen so far.
+If the current element is greater than maxRight, it is a leader.
 
-        int maxRight = arr[arr.length-1];
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
-        System.out.print("Leaders: " + maxRight + " ");
+class ArrayDemo8 {
 
-        for(int i = arr.length-2; i >=0; i--){
+    static void findLeaders(int arr[]) {
 
-            if(arr[i] > maxRight){
+        int maxRight = arr[arr.length - 1];
+
+        System.out.print("Leaders : " + maxRight + " ");
+
+        for(int i = arr.length - 2; i >= 0; i--) {
+
+            if(arr[i] > maxRight) {
 
                 maxRight = arr[i];
 
                 System.out.print(maxRight + " ");
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {16,17,4,3,5,2};
+
+        findLeaders(arr);
     }
 }
