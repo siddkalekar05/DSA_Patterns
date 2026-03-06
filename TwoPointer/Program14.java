87687b325183f4f7657467274211306d9c538cb3
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-06 23:32:31 +0530

    Add files via upload

---
+++ TwoPointer/Program14.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class PalindromeString {
+
+    static void check(String str){
+
+        int left = 0;
+        int right = str.length()-1;
+
+        boolean isPalindrome = true;
+
+        while(left < right){
+
+            if(str.charAt(left) != str.charAt(right)){
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if(isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String str = sc.nextLine();
+        check(str);
+    }
+}
