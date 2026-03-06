d9f384ce4026744d1efbad6bd4a0df88d1134b95
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-06 23:31:15 +0530

    Add files via upload

---
+++ TwoPointer/Program11.java
@@ -0,0 +1,20 @@
+class CompareStrings {
+
+    static void compare(String s1,String s2){
+
+        s1 = s1.replace(" ","");
+        s2 = s2.replace(" ","");
+
+        if(s1.equals(s2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+
+    public static void main(String[] args){
+
+        compare("hello world","helloworld");
+    }
+} 
+    
+
