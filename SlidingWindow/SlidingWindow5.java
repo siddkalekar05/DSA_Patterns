f8d4e1589605a7b38be2504889559f7b1b1b5ed2
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-07 21:30:57 +0530

    Add files via upload

---
+++ SlidingWindow/SlidingWindow5.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class VowelSubstring {
+
+    static boolean isVowel(char c){
+
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+
+    static void countVowels(String str, int k){
+
+        int count = 0;
+
+        for(int i=0;i<k;i++)
+            if(isVowel(str.charAt(i)))
+                count++;
+
+        System.out.println(count);
+
+        for(int i=k;i<str.length();i++){
+
+            if(isVowel(str.charAt(i)))
+                count++;
+
+            if(isVowel(str.charAt(i-k)))
+                count--;
+
+            System.out.println(count);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String str = sc.nextLine();
+        countVowels(str,3);
+    }
+}
