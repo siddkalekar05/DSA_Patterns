12ff959f5d205df2726f7f6fbe8cdf71d10882d6
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-07 21:30:28 +0530

    Add files via upload

---
+++ SlidingWindow/SlidingWindow3.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class SlidingWindow3{
+    static void window(int arr[],int k){
+       double avg = 0.0;
+       for(int i=0;i<k;i++){
+        avg+=arr[i];
+       }
+       System.out.println("avg of window = "+avg/k);
+       for(int i=k;i<arr.length;i++){
+            avg+=arr[i];
+            avg-=arr[i-k];
+            System.out.println(avg/k);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[]{1,3,2,6,-1,4,1,8,2};
+        System.out.println("Enter size of window : ");
+        int k = sc.nextInt();
+        window(arr,k);
+    }
+}
