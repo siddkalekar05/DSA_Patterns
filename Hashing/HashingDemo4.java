b45851eb4d3f4a00238e15708a298403697fab8a
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-09 21:52:15 +0530

    Add files via upload

---
+++ Hashing/HashingDemo4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class ContainsDuplicate {
+
+    static boolean check(int arr[]){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : arr){
+
+            if(set.contains(num))
+                return true;
+
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        int arr[] = {1,2,3,4,1};
+
+        System.out.println(check(arr));
+    }
+}
