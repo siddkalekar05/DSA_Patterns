17227e9f714c52cb3a540f335769db608165e306
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-11 22:45:18 +0530

    Add files via upload

---
+++ Kadane's_algo/KadaneDemo6.java
@@ -0,0 +1,40 @@
+class KadaneIndices {
+
+    static void maxSubarray(int arr[]){
+
+        int maxSum = arr[0];
+        int currentSum = 0;
+
+        int start = 0;
+        int end = 0;
+        int tempStart = 0;
+
+        for(int i=0;i<arr.length;i++){
+
+            currentSum += arr[i];
+
+            if(currentSum > maxSum){
+
+                maxSum = currentSum;
+                start = tempStart;
+                end = i;
+            }
+
+            if(currentSum < 0){
+
+                currentSum = 0;
+                tempStart = i + 1;
+            }
+        }
+
+        System.out.println("Max Sum: "+maxSum);
+        System.out.println("Subarray Index: "+start+" to "+end);
+    }
+
+    public static void main(String[] args){
+
+        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
+
+        maxSubarray(arr);
+    }
+}
