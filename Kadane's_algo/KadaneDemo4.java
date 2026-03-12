ac43bc588fc74bf6e1512a57054c4a170e75f94c
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-12 22:28:50 +0530

    Update KadaneDemo4.java

--- Kadane's_algo/KadaneDemo4.java
+++ Kadane's_algo/KadaneDemo4.java
@@ -1,3 +1,23 @@
+/*
+Problem Statement:
+Find maximum sum of a contiguous subarray.
+
+Example:
+Input: [1,-2,3,5,-1,2]
+Output: 9
+
+Subarray:
+[3,5,-1,2]
+
+Approach:
+Add elements to currentSum.
+If currentSum becomes negative, reset it to 0
+because negative sum will reduce future sums.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class KadaneExample4 {
 
     static void maxSum(int arr[]){
@@ -24,4 +44,5 @@ class KadaneExample4 {
 
         maxSum(arr);
     }
+
 }
