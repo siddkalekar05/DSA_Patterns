b181d09d173c6aace4c3456f7b4fc7d5bb5f0bd0
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-16 22:30:45 +0530

    Add files via upload

---
+++ Sorting/Bubble.java
@@ -0,0 +1,23 @@
+class Bubble {
+    static void sort2(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+               
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[]{5,3,8,4,2};
+        sort2(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
