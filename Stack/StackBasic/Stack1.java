2c13d7dd907da84cd47837aea5238a072138c494
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-20 21:39:21 +0530

    Add files via upload

---
+++ Stack/StackBasic/Stack1.java
@@ -0,0 +1,22 @@
+class StackPush {
+
+    int arr[] = new int[5];
+    int top = -1;
+
+    void push(int x){
+
+        top++;          
+        arr[top] = x;   
+    }
+
+    public static void main(String[] args){
+
+        StackPush s = new StackPush();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        System.out.println("Top: " + s.arr[s.top]);
+    }
+}
