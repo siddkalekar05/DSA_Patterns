1b47c73a78894ba50fa1efcaae1f9de5d0b4e7c0
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-22 22:12:37 +0530

    Add files via upload

---
+++ Stack/StackDemo7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class ReverseNumber3 {
+
+    static int reverse1(int num){
+
+        Stack<Integer> st = new Stack<>();
+
+        if(num == 0) return 0;
+        String str = Integer.toString(num);
+        for(int i = 0; i < str.length(); i++){
+            st.push(str.charAt(i) - '0');
+        }
+
+        int rev = 0;
+
+        while(!st.isEmpty()){
+            rev = rev * 10 + st.pop();
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter num : ");
+        int num = sc.nextInt();
+
+        int result = reverse1(num);
+        System.out.println("Reversed: " + result);
+    }
+}
