7ea2d723346984b81f431801c27304c3af070b79
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-22 21:23:41 +0530

    Add files via upload

---
+++ Stack/StackDemo3.java
@@ -0,0 +1,96 @@
+import java.util.*;
+class Stack1{
+    int maxsize;
+    int top = -1;
+    int stackarr[];
+    Stack1(int maxsize){
+        this.maxsize = maxsize;
+        stackarr = new int[maxsize];
+    }
+    void push(int data){
+        if(top==maxsize-1){
+            System.out.println("Stack is overflow");
+            return;
+        }else{
+            top++;
+            stackarr[top] = data;
+        }
+
+    }
+    int pop(){
+        int value = stackarr[top];
+        top--;
+        return value;
+
+    }
+    int peek(){
+        return stackarr[top];
+    }
+    boolean empty(){
+        if(top==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void printStack(){
+        for(int i = top;i>-1;i--){
+            System.out.print(stackarr[i]+" ");
+        }
+        System.out.println();
+    }
+}
+
+class StackDemo3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int size = sc.nextInt();
+        Stack1 st = new Stack1(size);
+        char ch;
+        do{
+            System.out.println("1.Push");
+            System.out.println("2.Pop");
+            System.out.println("3.Peek");
+            System.out.println("4.Empty ?");
+            System.out.println("5.Print");
+            System.out.println("Enter your choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:{
+                    System.out.println("Enter data : ");
+                    int data  = sc.nextInt();
+                    st.push(data);
+                    break;
+                }
+                case 2:{
+                    int value = st.pop();
+                    System.out.println("remove : "+value);
+                    break;
+                }
+                case 3:{
+                    int peek = st.peek();
+                    System.out.println("Peek ele : "+peek);
+                    break;
+                }
+                case 4 : {
+                    System.out.println("Stack is empty ? "+st.empty());
+                    break;
+                }
+                case 5 :{
+                    System.out.println("Stack ele : ");
+                    st.printStack();
+                    break;
+                }
+               
+                default:
+                    System.out.println("Enter valid choice ");
+                    break;
+            }
+            System.out.println("Do you want to continue(y/any char)?");
+            ch = sc.next().charAt(0);
+
+        }while(ch=='y'||ch=='Y');
+    }
+    
+}
