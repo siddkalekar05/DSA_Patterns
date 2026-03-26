ba3adf34433db107ecfc807c7db7a13c82080309
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-26 22:51:44 +0530

    Update QueueDemo3.java

--- Queue/QueueDemo3.java
+++ Queue/QueueDemo3.java
@@ -1,3 +1,20 @@
+/*
+Problem Statement:
+Remove element from front of queue.
+
+Example:
+Queue: [10,20,30]
+Output: 10 removed
+
+Approach (Steps):
+1. Access element at front
+2. Print/remove it
+3. Increment front
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
 class PeekDemo {
 
     int arr[] = {10,20,30};
