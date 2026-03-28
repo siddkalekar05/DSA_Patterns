faba159cf777a7e563be4aa1e733e6f2778b2e74
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-28 23:22:23 +0530

    Update Queue2.java

--- Queue/Queue2.java
+++ Queue/Queue2.java
@@ -1,3 +1,20 @@
+/*
+Problem Statement:
+Reverse elements of a queue.
+
+Example:
+Input: 10 20 30
+Output: 30 20 10
+
+Approach:
+Use stack:
+1. Push all queue elements into stack
+2. Pop from stack and enqueue back
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
 import java.util.*;
 
 class QueueUsingStack {
