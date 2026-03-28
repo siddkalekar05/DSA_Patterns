562b607ff3e069356a9345c4daf7a1f0ee810a1a
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-28 23:24:33 +0530

    Update Queue6.java

--- Queue/Queue6.java
+++ Queue/Queue6.java
@@ -1,3 +1,19 @@
+/*
+Problem Statement:
+Interleave first half with second half.
+
+Example:
+Input: 1 2 3 4
+Output: 1 3 2 4
+
+Approach:
+Split queue into halves and merge.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+
 import java.util.*;
 
 class BinaryQueue {
