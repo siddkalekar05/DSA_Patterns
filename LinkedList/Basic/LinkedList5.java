debafe2aa736c885286a603526aea455c42a0c00
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-29 23:00:38 +0530

    Add files via upload

---
+++ LinkedList/Basic/LinkedList5.java
@@ -0,0 +1,44 @@
+class InsertEnd {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    static Node insertEnd(Node head, int x){
+
+        Node newNode = new Node(x);
+
+        if(head == null)
+            return newNode;
+
+        Node temp = head;
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+
+        return head;
+    }
+
+    public static void main(String[] args){
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+
+        head = insertEnd(head,30);
+
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
