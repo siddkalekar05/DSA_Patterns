e6d615b9fd3b2335a15624b9b3df2fa9f7ed24df
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-29 22:58:22 +0530

    Add files via upload

---
+++ LinkedList/Basic/LinkedList2.java
@@ -0,0 +1,24 @@
+class CreateList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+
+        n1.next = n2;
+        n2.next = n3;
+
+        System.out.println(n1.data + " -> " + n1.next.data);
+    }
+}
